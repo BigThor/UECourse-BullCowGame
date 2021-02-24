4bca7e9c834ec8c8bf11af7ce8161760c225fffe
VICTOR ADRIAN SUAREZ RUIZ <A229520@alumnos.uaslp.mx>
2021-02-24 14:27:55 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# BullCowGame
+
+Practice project to learn the basics of Unreal Engine
+Course followed is: https://www.udemy.com/course/unrealcourse/
---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine(TEXT("Hi, and welcome to the game \"BULLS & COWS\""));
+    PrintLine(TEXT("Please, hit enter to continue..."));
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+    FString HiddenWord = TEXT("arbol");
+}
